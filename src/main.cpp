3deeee462507f2e942dc0a3caae0d603598dad7a
Juan Adonai Martínez Meza <a24110195@live.ceti.mx>
2026-04-25 03:03:28 +0000

    Practica 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-int main(int argc, char const *argv[]){
-    
+#include <SerieFocos.hpp>
+#include <Foco.hpp>
+
+int main(int argc, char const *argv[])
+{
+    SerieFocos serie;
+    serie.EncenderFocos();
+    serie.MostrarFocos();
+    serie.ApagarFocos();
+    serie.MostrarFocos();
+
     return 0;
 }

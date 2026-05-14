8040a62f44155a02e8cc0e5a90f4fcff583b7b0a
Juan Adonai Martínez Meza <a24110195@live.ceti.mx>
2026-05-14 00:49:58 +0000

    Practica 5

--- src/Memoria.cpp
+++ src/Memoria.cpp
@@ -5,5 +5,11 @@ int main(int argc, char const *argv[])
 {
     int hola;
     cout<<&hola<<endl;
+    
+    cout<<size of (int)<<end;
+    cout<<size of (char)<<end;
+    cout<<size of (bool)<<end;
+    cout<<size of (float)<<end;
+    
     return 0;
 }

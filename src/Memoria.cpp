edfd73672483b0dbfd1d6cce84069111814df06e
Juan Adonai Martínez Meza <a24110195@live.ceti.mx>
2026-05-13 20:29:50 -0600

    Práctica 5 (actualización)

--- src/Memoria.cpp
+++ src/Memoria.cpp
@@ -6,10 +6,10 @@ int main(int argc, char const *argv[])
     int hola;
     cout<<&hola<<endl;
     
-    cout<<size of (int)<<end;
-    cout<<size of (char)<<end;
-    cout<<size of (bool)<<end;
-    cout<<size of (float)<<end;
+    cout << sizeof(int) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(bool) << endl;
+    cout << sizeof(float) << endl;
     cout << sizeof(short) << endl;
     cout << sizeof(long) << endl;
     cout << sizeof(long long) << endl;
